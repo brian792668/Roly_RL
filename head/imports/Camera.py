da1f7480f9266dc6df0b8879448ae7af166d56e6
BrianLL <brian792668gm@gmail.com>
2024-09-18 16:33:47 +0800

    first commit

---
+++ head/imports/Camera.py
@@ -0,0 +1,74 @@
+import cv2
+import numpy as np
+
+class Camera():
+    def __init__(self, renderer, camID):
+        self.renderer = renderer
+        self.camID = camID
+        self.rgbimg = np.zeros((240, 640, 3), dtype=np.uint8)
+        self.depthimg = np.zeros((240, 640), dtype=np.uint8)
+        self.target = [0.0, 0.0]
+        self.target_depth = float('nan')
+
+    def get_img(self, data, rgb=True, depth=True):
+        if rgb == True:
+            self.renderer.disable_depth_rendering()
+            self.renderer.disable_segmentation_rendering()
+            self.renderer.update_scene(data, camera=self.camID)
+            self.rgbimg = self.renderer.render()
+        if depth == True:
+            self.renderer.enable_depth_rendering()
+            self.renderer.update_scene(data, camera=self.camID)
+            self.depthimg = self.renderer.render()
+            # self.depthimg = (255*np.clip(depth, 0, 1)).astype(np.uint8)
+
+    def show(self, rgb = False, depth = False):
+        if rgb == True:
+            cv2.imshow("RGB", cv2.cvtColor(self.rgbimg, cv2.COLOR_RGB2BGR))
+        if depth == True:
+            cv2.imshow("Depth", cv2.cvtColor(self.depthimg, cv2.COLOR_RGB2BGR))
+        cv2.waitKey(1)
+
+    def get_target(self):
+        # 定義紅色的RGB範圍
+        lower_red = np.array([100, 0, 0], dtype=np.uint8)
+        upper_red = np.array([255, 50, 50], dtype=np.uint8)
+        # 創建紅色遮罩
+        mask = cv2.inRange(self.rgbimg, lower_red, upper_red)
+
+        # 確保在圖像中有紅色物體
+        if np.any(mask):
+            # 獲取紅色物體的像素坐標
+            coords = np.column_stack(np.where(mask > 0))
+
+            # 計算紅色物體的中心點
+            center = np.mean(coords, axis=0)
+            center_y, center_x = center
+
+            # 在RGB圖像上畫十字標
+            cross_size = 5  # 十字標的大小
+            color = (255, 255, 255)  # 綠色
+            thickness = 1  # 線條的粗細
+
+            # 畫橫線
+            cv2.line(self.rgbimg, (int(center_x) - cross_size, int(center_y)), 
+                     (int(center_x) + cross_size, int(center_y)), color, thickness)
+            # 畫縱線
+            cv2.line(self.rgbimg, (int(center_x), int(center_y) - cross_size), 
+                     (int(center_x), int(center_y) + cross_size), color, thickness)
+
+            # 從深度圖像中獲取對應的深度值
+            self.target_depth = 100*self.depthimg[int(center_y), int(center_x)]
+
+            cv2.putText(self.rgbimg, f"{self.target_depth:.1f}", (int(center_x) + 10, int(center_y)), cv2.FONT_HERSHEY_SIMPLEX, 
+                    0.5, (255, 255, 255), 1, cv2.LINE_AA)
+
+            # 將像素座標轉換至[-1, 1]區間
+            norm_x = (center_x / self.rgbimg.shape[1]) * 2 - 1
+            norm_y = (center_y / self.rgbimg.shape[0]) * 2 - 1
+            self.target = [norm_x, norm_y]
+
+        else:
+            # 若無紅色物體，返回None並設置target為無效值
+            self.target = [float('nan'), float('nan')]
+            self.target_depth = float('nan')
---
+++ head/main3.py
@@ -0,0 +1,89 @@
+# 往外轉為正
+# 往外抬為正
+# 往前踢為正
+
+import mujoco
+import mujoco.viewer
+import numpy as np
+import os
+
+from imports.Settings import *
+from imports.Controller import *
+from imports.Draw_joint_info import *
+from imports.Show_camera_view import *
+from imports.Camera import *
+
+
+if __name__ == '__main__':
+    # Add xml path
+    current_dir = os.getcwd()
+    xml_path = 'RL/RolyURDF2/Roly.xml'
+    xml_path = os.path.join(current_dir, xml_path)
+
+    model = mujoco.MjModel.from_xml_path(xml_path)
+    data = mujoco.MjData(model)
+    renderer = mujoco.Renderer(model)
+
+
+    # initialize
+    pos = initPos
+    target = initTarget
+    # sensor = initSensor
+
+    # set PID controller parameter
+    PIDctrl = PIDcontroller(controlParameter, target)
+
+    # if base is free joint
+    data.qpos[:] = pos[:]
+    # # if base is fixed
+    # data.qpos[:] = pos[7:]
+
+    head_camera = Camera(renderer=renderer, camID=0)
+
+    mujoco.mj_resetData(model, data)  # Reset state and time.
+    with mujoco.viewer.launch_passive(model, data) as viewer:
+        # Close the viewer automatically after 30 wall-seconds.
+        step = 0
+        while viewer.is_running():
+            step += 1
+
+            # Get robot simu data
+            ## if base is free joint
+            pos = [data.qpos[i] for i in controlList]
+            vel = [data.qvel[i-1] for i in controlList]
+            # if base is not free joint
+            # pos = [data.qpos[i-7] for i in controlList]
+            # vel = [data.qvel[i-8] for i in controlList]
+
+            target[0]=  0.20*(np.cos(step*2*np.pi/8000)-1) # trunk
+            target[1]= -0.20*(np.cos(step*2*np.pi/8000)-1) # neck
+            target[2]=  0.30*(np.cos(step*2*np.pi/8000)-1) # camera
+
+            target[3]  = -0.20*(np.cos(step*2*np.pi/8000)-1) # R shoulder
+            target[3+1]=  1.00*(np.cos(step*2*np.pi/8000)-1) # R arm1
+            target[3+2]= -1.00*(np.cos(step*2*np.pi/8000)-1) # R arm2
+            # target[3+3]= -0.50*(np.cos(step*2*np.pi/8000)-1) # R arm3
+            # target[3+4]= -0.50*(np.cos(step*2*np.pi/8000)-1) # R arm4
+            # target[3+5]= -0.50*(np.cos(step*2*np.pi/8000)-1) # R palm
+            target[3+6]= -0.01*(np.cos(step*2*np.pi/8000)-1) # R gripper
+
+            target[10]  = -0.20*(np.cos(step*2*np.pi/8000)-1) # L shoulder
+            target[10+1]= -1.00*(np.cos(step*2*np.pi/8000)-1) # L arm1
+            target[10+2]=  1.00*(np.cos(step*2*np.pi/8000)-1) # L arm2
+            # target[10+3]=  0.50*(np.cos(step*2*np.pi/8000)-1) # L arm3
+            # target[10+4]= -0.50*(np.cos(step*2*np.pi/8000)-1) # L arm4
+            # target[10+5]= -0.50*(np.cos(step*2*np.pi/8000)-1) # L palm
+            target[10+6]= -0.01*(np.cos(step*2*np.pi/8000)-1) # L gripper
+
+            data.ctrl[:] = PIDctrl.getSignal(pos, vel, target)
+            mujoco.mj_step(model, data)
+
+            if step%100 == 0:
+                viewer.sync()
+                head_camera.get_img(data, rgb=True, depth=True)
+                head_camera.get_target()
+                head_camera.show(rgb=True, depth=True)
+
+
+        renderer.close() 
+        cv2.destroyAllWindows() 
