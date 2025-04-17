1710b96feab0f5fd636da8d5886bf84b10034fac
BrianLL <brian792668gm@gmail.com>
2025-04-17 15:57:22 +0800

    Clean commit without large files

--- Roly/imports/Forward_kinematics.py
+++ Roly/imports/Forward_kinematics.py
@@ -3,6 +3,9 @@ import numpy as np
 class DHtable():
     def __init__(self, table):
         self.table = table
+    
+    def update_hand_length(self, hand_length):
+        self.table[-1][3] = 0.17 + hand_length
 
     def Tans_Matrix(self, link_number, angle):
         [theta, alpha, a, d] = self.table[link_number]
