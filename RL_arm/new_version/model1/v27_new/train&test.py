e48eb7978a71a8b426258524ed9316fac9f8f416
BrianLL <brian792668gm@gmail.com>
2025-04-17 16:13:26 +0800

    up to date

--- RL_arm/new_version/model1/v27_new/train&test.py
+++ RL_arm/new_version/model1/v27_new/train&test.py
@@ -94,5 +94,5 @@ if __name__ == '__main__':
         RL_model.save(current_model_path)
 
     # train(RL_model, my_env, file_path, render_speed = 1.0)
-    test(RL_model, my_env, current_model_path, render_speed = 0.1)
-    # test(RL_model, my_env, best_model_path, render_speed = 0.1)
+    # test(RL_model, my_env, current_model_path, render_speed = 0.1)
+    test(RL_model, my_env, best_model_path, render_speed = 0.1)
